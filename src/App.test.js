5fca059a4b0b11e39baca11968ee06ae5bd73695
SusanBird <susanbird391@gmail.com>
2022-04-29 10:01:19 -0700

    testing error

--- src/App.test.js
+++ src/App.test.js
@@ -3,6 +3,6 @@ import App from './App';
 
 test('renders learn react link', () => {
   render(<App />);
-  const linkElement = screen.getByText(/alien/i);
+  const linkElement = screen.getByText(/'alien'/i);
   expect(linkElement).toBeInTheDocument();
 });
